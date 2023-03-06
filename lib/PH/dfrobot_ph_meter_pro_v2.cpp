6f1fd6b6184234de4284fc5bcb90fe4661cfdb47
Emil Zublin <ejzublin@gmail.com>
2023-03-06 14:43:50 -0800

    Finished renaming all real's

--- lib/PH/dfrobot_ph_meter_pro_v2.cpp
+++ lib/PH/dfrobot_ph_meter_pro_v2.cpp
@@ -3,25 +3,25 @@
 #include "esp_adc_cal.h"
 #include "pins.hpp"
 
-RealPH::RealPH(esp_adc_cal_characteristics_t adc1_chars) : ph_read_pin{PH_READ_PIN}, adc1_chars{adc1_chars}
+DFRobotPHMeterProV2::DFRobotPHMeterProV2(esp_adc_cal_characteristics_t adc1_chars) : ph_read_pin{PH_READ_PIN}, adc1_chars{adc1_chars}
 {
     adc1_config_channel_atten(ph_read_pin, ADC_ATTEN_DB_11);
 }
 
-double RealPH::getPH()
+double DFRobotPHMeterProV2::getPH()
 {
     auto ph_mV = getPH_mV();
     auto ph = voltageToPH(ph_mV);
     return ph;
 }
 
-uint32_t RealPH::getPH_mV()
+uint32_t DFRobotPHMeterProV2::getPH_mV()
 {
     uint32_t ph_mV = esp_adc_cal_raw_to_voltage(adc1_get_raw(ph_read_pin), &this->adc1_chars);
     return ph_mV;
 }
 
-double RealPH::voltageToPH(uint32_t voltage)
+double DFRobotPHMeterProV2::voltageToPH(uint32_t voltage)
 {
     float slope = (7.0 - 4.0) / ((1500.0 - 1500.0) / 3.0 - (2032.44 - 1500.0) / 3.0);
     float intercept = 7.0 - slope * (1500.0 - 1500.0) / 3.0;
