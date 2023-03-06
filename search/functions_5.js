bb0f8ff34025a589e903dfc9b7a3469ada32cc80
limenilbuz <limenilbuz@users.noreply.github.com>
2023-03-06 19:05:56 +0000

    deploy: e7c5b6cb7bd32a739cad5f4c4dfcc59f2157298c

--- search/functions_5.js
+++ search/functions_5.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['getec_0',['getEC',['../classDFRobotECMeterPro.html#ae9cf21d7ebe23c75de34956564151e04',1,'DFRobotECMeterPro::getEC()'],['../classEC.html#ab20ab7ee80692277e2091f37ae6c9741',1,'EC::getEC()'],['../classMockEC.html#a4d1dd2b6ecdd49228f8c0d6f152c2886',1,'MockEC::getEC()']]],
   ['getec_5fmv_1',['getEC_mV',['../classDFRobotECMeterPro.html#ab8369f7472828648240a56e06a129a68',1,'DFRobotECMeterPro']]],
-  ['getph_2',['getPH',['../classMockPH.html#ada3b1b468b06b6f2278169b1d8103752',1,'MockPH::getPH()'],['../classPH.html#a3a704579e9fdd336f59c990a7009e92e',1,'PH::getPH()'],['../classRealPH.html#a84e95894778ed8b488e4a29e9be29513',1,'RealPH::getPH() override']]],
+  ['getph_2',['getPH',['../classRealPH.html#a84e95894778ed8b488e4a29e9be29513',1,'RealPH::getPH()'],['../classMockPH.html#ada3b1b468b06b6f2278169b1d8103752',1,'MockPH::getPH()'],['../classPH.html#a3a704579e9fdd336f59c990a7009e92e',1,'PH::getPH()']]],
   ['getph_5fmv_3',['getPH_mV',['../classRealPH.html#a8cf01db9fafd55d11ca10931a101bf5e',1,'RealPH']]],
   ['gettemp_5fmv_4',['getTemp_mV',['../classDFRobotECMeterPro.html#ada673bd636690e57e42d700d01a51f06',1,'DFRobotECMeterPro']]]
 ];
