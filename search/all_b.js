96407fcad465d1667dbf819fd0cc110d0a0fb1a3
limenilbuz <limenilbuz@users.noreply.github.com>
2023-03-06 03:38:48 +0000

    deploy: 547396a144a00d5b52208034240c9b631f152bb6

--- search/all_b.js
+++ search/all_b.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['test_0',['Test',['../classTest.html',1,'']]],
-  ['threshold_1',['Threshold',['../structThreshold.html',1,'']]],
-  ['toppm_2',['toPPM',['../classECMetric.html#a4bf261df3a0f1bf9ddb505cd728695db',1,'ECMetric']]]
+  ['threshold_0',['Threshold',['../structThreshold.html',1,'']]],
+  ['toppm_1',['toPPM',['../classECMetric.html#a4bf261df3a0f1bf9ddb505cd728695db',1,'ECMetric']]]
 ];
--- search/classes_7.js
+++ search/classes_7.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['test_0',['Test',['../classTest.html',1,'']]],
-  ['threshold_1',['Threshold',['../structThreshold.html',1,'']]]
+  ['threshold_0',['Threshold',['../structThreshold.html',1,'']]]
 ];
