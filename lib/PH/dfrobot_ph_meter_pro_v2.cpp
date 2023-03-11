971587f772784f16a0a26b86352853a4065aa59f
Emil Zublin <ejzublin@gmail.com>
2023-03-11 15:33:58 -0800

    Added pump task

--- lib/PH/dfrobot_ph_meter_pro_v2.cpp
+++ lib/PH/dfrobot_ph_meter_pro_v2.cpp
@@ -31,10 +31,10 @@ double DFRobotPHMeterProV2::voltageToPH(uint32_t voltage)
 
 double voltageToPH(uint32_t voltage, double temperature) {
     // https://images.hach.com/asset-get.download.jsa?id=17525673904
-    #define 25_CELSIUS 25
+    #define CELSIUS_25 25
     #define NORMALIZATION_VALUE 273.15   
     double slope_at_25C = (7.0 - 4.0) / ((1500.0 - 1500.0) / 3.0 - (2032.44 - 1500.0) / 3.0);
-    double slope_normalized = slope_at_25C * ((temperature + NORMALIZATION_VALUE) / (25_CELSIUS + NORMALIZATION_VALUE));
+    double slope_normalized = slope_at_25C * ((temperature + NORMALIZATION_VALUE) / (CELSIUS_25 + NORMALIZATION_VALUE));
     double value = 7 - (voltage / slope_normalized);
     return value;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,7 @@
 #include "dfrobot_ec_meter_pro.hpp"
 #include "pins.hpp"
 #include "driver/i2c.h"
+#include "pump.hpp"
 
 #define TASK_STACK_SIZE 4096
 #define TASK_PRIORITY 1
@@ -103,6 +104,50 @@ void ecTask(void *pvParameters)
     }
 }
 
+void pumpTask(void *pvParameters)
+{
+
+    /*
+    Jeon calibration of pumps states 0.31 mL/s
+    .31mL per second
+    1ml = 3.22 second
+    1ml = 3220 millsecond
+    */
+
+    int milliseconds_to_one_ml = 3220;
+
+    Pump ph_down_pump{milliseconds_to_one_ml, PH_DOWN_PUMP_RELAY_PIN};
+    Pump ph_up_pump{milliseconds_to_one_ml, PH_UP_PUMP_RELAY_PIN};
+    Pump ec_pump{milliseconds_to_one_ml, EC_PUMP_RELAY_PIN};
+
+    while (true)
+    {
+        double delta = 0.5;
+
+        if (!THRESHOLD.PUMP)
+        {
+            vTaskDelay(10000 / portTICK_PERIOD_MS); // only run every 100 seconds
+            continue;
+        }
+
+        if (system_measurements.PH > THRESHOLD.PH + delta)
+        {
+            ph_down_pump.activate(1);
+        }
+        else if (system_measurements.PH < THRESHOLD.PH - delta)
+        {
+            ph_up_pump.activate(1);
+        }
+
+        if (system_measurements.EC < THRESHOLD.EC - delta)
+        {
+            ec_pump.activate(1);
+        }
+
+        vTaskDelay(10000 / portTICK_PERIOD_MS); // only run every 10 seconds
+    }
+}
+
 // I2C setup
 static esp_err_t i2c_slave_init(void)
 {
@@ -127,7 +172,7 @@ void I2CReadTask(void *pvParameters)
 {
     while (true)
     {
-        i2c_slave_read_buffer(I2C_SLAVE_NUM, (uint8_t*)&THRESHOLD, I2C_SLAVE_RX_BUF_LEN, 100 / portTICK_PERIOD_MS);
+        i2c_slave_read_buffer(I2C_SLAVE_NUM, (uint8_t *)&THRESHOLD, I2C_SLAVE_RX_BUF_LEN, 100 / portTICK_PERIOD_MS);
         i2c_reset_rx_fifo(I2C_SLAVE_NUM);
 
         ESP_LOGI("PH", "THRESHOLD PH: %.2f", THRESHOLD.PH);
@@ -160,16 +205,33 @@ void I2CWriteTask(void *pvParameters)
         }
 
         system_measurements.Temperature = 60 + ((double)(rand() % 500) / 100);
-        
+
         double wl_threshold = rand() % 100;
         system_measurements.WaterLevel = false;
         if (wl_threshold > 10){
             system_measurements.WaterLevel = true;
-            
+
         }
         */
-        i2c_slave_write_buffer(I2C_SLAVE_NUM, (uint8_t*)&system_measurements, sizeof(system_measurements), 100 / portTICK_PERIOD_MS);
+        i2c_slave_write_buffer(I2C_SLAVE_NUM, (uint8_t *)&system_measurements, sizeof(system_measurements), 100 / portTICK_PERIOD_MS);
         vTaskDelay(500 / portTICK_PERIOD_MS);
+    }
+}
+
+void relayTest(void *pvParameters)
+{
+    gpio_reset_pin(PH_UP_PUMP_RELAY_PIN);
+    gpio_set_direction(PH_UP_PUMP_RELAY_PIN, GPIO_MODE_OUTPUT);
+    Pump ph_up_pump{3220, PH_UP_PUMP_RELAY_PIN};
+    bool once = false;
+    while (1)
+    {
+        if (!once) {
+            ph_up_pump.activate(10);
+            once = true;
+        }
+
+        vTaskDelay(10000 / portTICK_PERIOD_MS);
     }
 }
 
@@ -189,9 +251,11 @@ extern "C" void app_main(void)
     gpio_set_direction(LED, GPIO_MODE_OUTPUT);
     char *taskName = pcTaskGetName(NULL);
 
-    // xTaskCreate(ecTask, "ec task", TASK_STACK_SIZE, NULL, TASK_PRIORITY, NULL);
-    // xTaskCreate(waterLevelTask, "water level task", TASK_STACK_SIZE, NULL, TASK_PRIORITY, NULL);
+    xTaskCreate(ecTask, "ec task", TASK_STACK_SIZE, NULL, TASK_PRIORITY, NULL);
+    xTaskCreate(waterLevelTask, "water level task", TASK_STACK_SIZE, NULL, TASK_PRIORITY, NULL);
     xTaskCreate(pHTask, "ph task", TASK_STACK_SIZE, NULL, TASK_PRIORITY, NULL);
     xTaskCreate(I2CReadTask, "I2C Read", TASK_STACK_SIZE, NULL, TASK_PRIORITY, NULL);
     xTaskCreate(I2CWriteTask, "I2C Write", TASK_STACK_SIZE, NULL, TASK_PRIORITY, NULL);
+    //xTaskCreate(pumpTask, "Pump array task", TASK_STACK_SIZE, NULL, TASK_PRIORITY - 1, NULL);
+    //xTaskCreate(relayTest, "asdf", TASK_STACK_SIZE, NULL, TASK_PRIORITY, NULL);
 }
