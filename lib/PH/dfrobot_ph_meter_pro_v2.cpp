08b1344490fbef93f038f6c084626397af209508
Emil Zublin <80178962+limenilbuz@users.noreply.github.com>
2023-03-16 17:01:17 -0700

    Update dfrobot_ph_meter_pro_v2.cpp
    Added reference to DFRobot PH library

--- lib/PH/dfrobot_ph_meter_pro_v2.cpp
+++ lib/PH/dfrobot_ph_meter_pro_v2.cpp
@@ -32,6 +32,7 @@ uint32_t DFRobotPHMeterProV2::getPH_mV()
 
 double DFRobotPHMeterProV2::voltageToPH(uint32_t voltage)
 {
+    // adapted from: https://github.com/DFRobot/DFRobot_PH/blob/master/DFRobot_PH.cpp
     float slope = (7.0 - 4.0) / ((neutral_ph_mv - 1500.0) / 3.0 - (acidic_ph_mv - 1500.0) / 3.0);
     float intercept = 7.0 - slope * (neutral_ph_mv - 1500.0) / 3.0;
     double value = slope * (voltage - 1500.0) / 3.0 + intercept;
@@ -42,7 +43,7 @@ double voltageToPH(uint32_t voltage, double temperature) {
     // https://images.hach.com/asset-get.download.jsa?id=17525673904
     #define CELSIUS_25 25
     #define NORMALIZATION_VALUE 273.15   
-    double slope_at_25C = (7.0 - 4.0) / ((1500.0 - 1500.0) / 3.0 - (2032.44 - 1500.0) / 3.0);
+    double slope_at_25C = (7.0 - 4.0) / ((neutral_ph_mv - 1500.0) / 3.0 - (acidic_ph_mv - 1500.0) / 3.0);
     double slope_normalized = slope_at_25C * ((temperature + NORMALIZATION_VALUE) / (CELSIUS_25 + NORMALIZATION_VALUE));
     double value = 7 - (voltage / slope_normalized);
     return value;
